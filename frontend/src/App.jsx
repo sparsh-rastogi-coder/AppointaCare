f9159d18504da4e0d47c4e6f5772b05778a89ac1
sparsh-rastogi-coder <sparshrastogiclass11a@gmail.com>
2025-06-24 23:18:18 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,6 +48,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/doctors' element={<Doctors />} />
+          <Route path='/doctors/:speciality' element={<Doctors />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/login' element={<Login />} />
