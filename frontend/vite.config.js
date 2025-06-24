ddc8f00c328af951e8e8124016522a603ce0faa7
sparsh-rastogi-coder <sparshrastogiclass11a@gmail.com>
2025-06-24 14:55:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://appointa-care.vercel.app/',
         changeOrigin: true
       }
     }
