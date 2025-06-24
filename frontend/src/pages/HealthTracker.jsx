eaf6337ef6b82599c29408f52f1f199c42bc407b
sparsh-rastogi-coder <sparshrastogiclass11a@gmail.com>
2025-06-24 18:07:05 +0530

    Update HealthTracker.jsx

--- frontend/src/pages/HealthTracker.jsx
+++ frontend/src/pages/HealthTracker.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect ,useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import HealthMetricsForm from '../components/HealthTracker/HealthMetricsForm';
